9d31b761e41cec53e4bb99f7ffb7e7af60c435af
HarshitaGupta0512 <98079342+HarshitaGupta0512@users.noreply.github.com>
2022-05-28 01:44:38 +0530

    neww

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ import pickle
 
 
 # load the nlp model and tfidf vectorizer from disk
-filename = 'nlp_model.pkl'
-clf = pickle.load(open(filename, 'rb'))
-vectorizer = pickle.load(open('tranform.pkl','rb'))
+# filename = 'nlp_model.pkl'
+# clf = pickle.load(open(filename, 'rb'))
+# vectorizer = pickle.load(open('tranform.pkl','rb'))
 
 def create_similarity():
     data = pd.read_csv('main_data.csv')
